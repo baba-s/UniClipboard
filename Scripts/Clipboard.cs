ee39bd822d99f4b6edbf3b98f86acc77d11d4e3d
baba_s <shotababa1206@gmail.com>
2020-04-21 20:45:28 +0900

    first commit

---
+++ Scripts/Clipboard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace UniClipboard
+{
+	public static class Clipboard
+	{
+		public static string GetText()
+		{
+			return GUIUtility.systemCopyBuffer;
+		}
+
+		public static void SetText( string text )
+		{
+			GUIUtility.systemCopyBuffer = text;
+		}
+
+		public static void SetText<T>( T target )
+		{
+			SetText( target.ToString() );
+		}
+	}
+}
